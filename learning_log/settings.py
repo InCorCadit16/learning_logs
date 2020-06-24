27986ae6db8134577084ead6a5550b179c2e77af
InCorCadit16 <incorcadit16@gmail.com>
2020-06-24 15:52:01 +0300

    Added bootstrap4

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # Third party apps
+    'bootstrap4',
+
     # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,3 +129,8 @@ STATIC_URL = '/static/'
 
 # My settings
 LOGIN_URL = 'users:login'
+
+
+# Heroku settings
+import django_heroku
+django_heroku.settings(locals())
